d4efa1d25bbb459fdb00ee85b1f505a190dee301
msadraei <msadraei@icloud.com>
2019-05-24 19:22:50 +0430

    :/

--- 4/3.py
+++ 4/3.py
@@ -1,32 +1,29 @@
 n,m = [int(x) for x in input().split()]
-neighbors = [[] for i in range(n)]
+edges = [[0]*(n+2) for i in range(n+2)]
 votes = [int(x) for x in input().split()]
-edges = []
-final_votes = votes[:]
 for i in range(m):
     a,b = [int(x) for x in input().split()]
-    a = a-1
-    b = b-1
-    neighbors[a].append(b)
-    neighbors[b].append(a)
-    edges.append((a,b))
-def calc_most_neigbors(index):
-    counter = [0,0]
-    counter[votes[index]]+=1
-    for item in neighbors[index]:
-        counter[final_votes[item]]+=1
-    return counter.index(max(counter))
-for i in range(n):
-    back_f = final_votes[:]
-    for i in range(n):
-        final_votes[i] = calc_most_neigbors(i)
-    if back_f == final_votes:
+    edges[a][b] = 1
+    edges[b][a] = 1
+for index,item in enumerate(votes):
+    if item == 1:
+        edges[0][index+1] = 1
+    else:
+        edges[index+1][-1] = 1
+def bfs():
+    que = [0]
+    all_parent = [-1]*(n+2)
+    visited    = [False]*(n+2)
+    while True:
+        item = que.pop(0)
+        for next_v,check in enumerate(votes):
+            if check>0 and not visited[next_v]:
+                que.append(next_v)
+                visited[next_v] = True
+                all_parent[next_v] = item
+    return visited[-1],all_parent
+while True:
+    check,path = bfs()
+    if not check:
         break
-cost = 0
-for i in range(n):
-    if final_votes[i] != votes[i]:
-        cost += 1
-for edge in edges:
-    if final_votes[edge[0]] != final_votes[edge[1]]:
-        cost += 1
-print(cost)
+    
